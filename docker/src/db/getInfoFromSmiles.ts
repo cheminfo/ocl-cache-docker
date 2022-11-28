6a5d910ab8d774e7ff9ac356a2ded731673515b2
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-11-28 15:56:37 +0100

    fix: removed debug

--- docker/src/db/getInfoFromSmiles.ts
+++ docker/src/db/getInfoFromSmiles.ts
@@ -6,13 +6,9 @@ import { MoleculeInfo } from '../MoleculeInfo';
 import { getInfoFromMolecule } from './getInfoFromMolecule';
 
 const debug = debugLibrary('getInfoFromSmiles');
-let currentlyOpen = 0;
 
 export function getInfoFromSmiles(smiles: string): Promise<MoleculeInfo> {
   debug(smiles);
-  currentlyOpen++;
   const molecule = Molecule.fromSmiles(smiles);
-  currentlyOpen--;
-  debug('Currently open: ' + currentlyOpen);
   return getInfoFromMolecule(molecule);
 }
