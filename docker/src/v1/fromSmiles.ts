67e0f8df5ad83dec72a166305468cef3ac627d5a
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-11-28 16:22:20 +0100

    wip: remove debug

--- docker/src/v1/fromSmiles.ts
+++ docker/src/v1/fromSmiles.ts
@@ -5,7 +5,6 @@ import { FastifyInstance } from 'fastify/types/instance';
 import { getInfoFromSmiles } from '../db/getInfoFromSmiles';
 
 const debug = debugLibrary('getInfoFromSmiles');
-let currentlyOpen = 0;
 export default function fromSmiles(fastify: FastifyInstance) {
   fastify.get(
     '/v1/fromSmiles',
@@ -28,10 +27,7 @@ export default function fromSmiles(fastify: FastifyInstance) {
 async function getInfo(request: FastifyRequest, response: FastifyReply) {
   const body: any = request.query;
   try {
-    currentlyOpen++;
     const result = await getInfoFromSmiles(body.smiles);
-    currentlyOpen--;
-    debug(`currentlyOpen: ${currentlyOpen}`);
     return await response.send({ result });
   } catch (e: any) {
     debug(`Error: ${e.stack}`);
