660f8506ffb96ddc9bbb7a98f78e755bc3209b86
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-11-28 15:51:53 +0100

    wip: debug

--- docker/src/db/getInfoFromMolecule.ts
+++ docker/src/db/getInfoFromMolecule.ts
@@ -10,11 +10,9 @@ import Debug from 'debug';
 const debug = Debug('getInfoFromMolecule');
 
 let stmt: Statement;
-let currentlyOpen = 0;
 export async function getInfoFromMolecule(
   molecule: Molecule,
 ): Promise<MoleculeInfo> {
-  currentlyOpen++;
   const db = getDB();
   const idCode = molecule.getIDCode();
   if (!stmt) {
@@ -23,14 +21,10 @@ export async function getInfoFromMolecule(
   const result = stmt.get(idCode);
   if (result) {
     debug('in cache');
-    currentlyOpen--;
-    debug('Currently open: ' + currentlyOpen);
     return improve(result);
   }
 
   const secondResult = await improve(await insertMolecule(idCode, db));
-  currentlyOpen--;
-  debug('Currently open: ' + currentlyOpen);
   return secondResult;
 }
 
--- docker/src/db/getInfoFromSmiles.ts
+++ docker/src/db/getInfoFromSmiles.ts
@@ -6,9 +6,13 @@ import { MoleculeInfo } from '../MoleculeInfo';
 import { getInfoFromMolecule } from './getInfoFromMolecule';
 
 const debug = debugLibrary('getInfoFromSmiles');
+let currentlyOpen = 0;
 
 export function getInfoFromSmiles(smiles: string): Promise<MoleculeInfo> {
   debug(smiles);
+  currentlyOpen++;
   const molecule = Molecule.fromSmiles(smiles);
+  currentlyOpen--;
+  debug('Currently open: ' + currentlyOpen);
   return getInfoFromMolecule(molecule);
 }
