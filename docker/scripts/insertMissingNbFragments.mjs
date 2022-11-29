43cb4cda1801b790c86c044f82a19bf244281209
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-11-29 09:41:05 +0100

    wip: insert nbfragments

--- docker/scripts/insertMissingNbFragments.mjs
+++ docker/scripts/insertMissingNbFragments.mjs
@@ -1,8 +1,9 @@
+import { existsSync, mkdirSync } from 'fs';
+import { join } from 'path';
+
+import sqLite from 'better-sqlite3';
 import debugLibrary from 'debug';
 import OCL from 'openchemlib';
-
-// insert missing fragments in the database sqlite molecules table
-import getDB from '../src/db/getDB.js';
 
 const debug = debugLibrary('insertMissingNbFragments');
 
@@ -26,6 +27,46 @@ export default async function insertMissingNbFragments() {
       idCode,
     ]);
   }
+}
+
+export function getDB() {
+  let db;
+  // get __dirname
+  const __dirname = new URL('.', import.meta.url).pathname;
+  if (!db) {
+    const path = join(__dirname, '../../sqlite/');
+    if (!existsSync(path)) {
+      mkdirSync(path);
+    }
+    db = sqLite(join(path, 'db.sqlite'));
+    // https://www.sqlite.org/wal.html
+    // Activating WAL mode allows to get a speed improvement of 100x !!!
+    db.pragma('journal_mode = WAL');
+  }
+
+  const sql = `
+CREATE TABLE IF NOT EXISTS molecules (
+  idCode data_type PRIMARY KEY,
+  mf data_type TEXT,
+  em data_type REAL,
+  mw data_type REAL,
+  charge data_type INT,
+  noStereoID data_type TEXT,
+  noStereoTautomerID data_type TEXT,
+  logS data_type REAL,
+  nbFragments data_type INT,
+  logP data_type REAL,
+  acceptorCount data_type INT,
+  donorCount data_type INT,
+  rotatableBondCount data_type INT,
+  stereoCenterCount data_type INT,
+  polarSurfaceArea data_type REAL,
+  ssIndex data_type BLOB
+);
+`;
+
+  db.exec(sql);
+  return db;
 }
 
 await insertMissingNbFragments();
